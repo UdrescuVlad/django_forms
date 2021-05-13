79c0ed112cf88d4fb909f1a4d348278ce00d47ca
Vlad Udrescu <vlad.udrescu@everseen.com>
2021-05-13 11:08:39 +0300

    working with widgets

--- nadiasgarden-project/pizza/forms.py
+++ nadiasgarden-project/pizza/forms.py
@@ -1,14 +1,18 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 # class PizzaForm(forms.Form):
 #     topping1 = forms.CharField(label='Topping 1', max_length=100)
 #     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     # toppings = forms.MultipleChoiceField(choices=[('pep','Peperoni'),('cheese','Cheese'),('olives','Olives')],widget=forms.CheckboxSelectMultiple)
 #     size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')])
 
 # creating a model form
 
 class PizzaForm(forms.ModelForm):
+
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect())
+
     class Meta:
         model = Pizza
         fields = ['topping1', 'topping2', 'size']
